265c2cadadc65f9fdcdf61386530b9f2c70b59d8
Julian <julian@wheresnick.com.au>
2023-11-28 19:55:00 +1100

    finished adding seeder.js

--- seeders/db-seeder.js
+++ seeders/db-seeder.js
@@ -1,51 +1,73 @@
+
 // Import models 
+const sequelize = require('../config/connection');
 const { Club, Book, ClubBook, Review, UserBook, User } = require('../models');
 
 // Function to create a club
 const createClub = async () => {
-  await Club.create({ club_name: 'Old Margarets Gals' });
+  const club = await Club.create({ 
+    club_name: 'Old Margarets Gals' });
+    return club.id; // store club and return Id to use in seeding
 };
 
-// Function to create a book
+// Function to create a book - 
 const createBook = async () => {
-  await Book.create({ book_name: 'Then There Were None', author: 'Agatha Christie', description: 'Some people get murdered', genre: 'Detective' });
+  const book = await Book.create({ 
+    book_name: 'Then There Were None', 
+    author: 'Agatha Christie', 
+    description: 'Some people get murdered', 
+    genre: 'Detective' 
+  });
+  return book.id;
 };
 
 // Function to create a user
-const createUser = async () => {
-  await User.create({
+const createUser = async (clubId) => {
+  const user = await User.create({
     first_name: 'Margaret',
     last_name: 'Bucket',
     user_name: 'OldMarge',
     email: 'marge@aol.com',
     password: 'Potatosoup',
-    bookclub_id: 1,
+    bookclub_id: clubId,
   });
+  return user.id;
 };
 
 // Function to create a review
-const createReview = async () => {
-  await Review.create({ review_text: 'Another classic here girls', book_rating: 5, book_id: 1 });
+const createReview = async (bookId) => {
+  await Review.create({ 
+    review_text: 'Another classic here girls', 
+    book_rating: 5, 
+    book_id: bookId 
+  });
 };
 
 // Function to create clubbooks
-const createClubBook = async () => {
-  await ClubBook.create({ book_id: 1, club_id: 1 });
+const createClubBook = async (clubId, bookId) => {
+  await ClubBook.create({ book_id: bookId, club_id: clubId });
 };
 
 // Function to create userbooks
-const createUserBook = async () => {
-  await UserBook.create({ book_id: 1, user_id: 1 });
+const createUserBook = async (userId, bookId) => {
+  await UserBook.create({ book_id: bookId, user_id: userId });
 };
 
 // Function to populate the database
 const seed = async () => {
-  await createClub();
-  await createBook();
-  await createUser();
-  await createReview();
-  await createClubBook();
-  await createUserBook();
+  try {
+  const clubId = await createClub();
+  const bookId = await createBook();
+  const userId = await createUser(clubId); // store user ID returned from createUser
+
+  await createReview(bookId); // use stored data
+  await createClubBook(clubId, bookId);
+  await createUserBook(userId, bookId);
+
+  console.log('Database seeded successfully');
+} catch (error) {
+  console.error('Error seeding database:', error);
+}
 };
 
 module.exports = seed;
