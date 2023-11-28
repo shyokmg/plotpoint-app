856dd1690f8d969e5811baea8a594e9b1fbb9810
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-11-28 18:54:53 +0900

    Models, Controllers - Updated User model and updated userroutes and homeroute, and added club data in view

--- models/Review.js
+++ models/Review.js
@@ -22,13 +22,13 @@ Review.init(
             msg: 'Please Rate Book from 1 to 5 Stars',
         }
     }},
-    // user_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //       model: 'user',
-    //       key: 'id',
-    //   }
-    // },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'user',
+          key: 'id',
+      }
+    },
     book_id: {
       type: DataTypes.INTEGER,
       references: {
